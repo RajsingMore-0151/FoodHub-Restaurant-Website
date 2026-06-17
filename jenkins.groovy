1b092a21cc14fa41f50d4ef8d9bc9e57bc4a0cba
RajsingMore-0151 <rajsingjmore151@gmail.com>
2026-06-17 13:09:47 +0530

    updated jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,13 +2,13 @@ pipeline {
     agent any
 
     environment {
-        GIT_REPO      = 'https://github.com/RajsingMore-0151/ShopEase-Website.git'
+        GIT_REPO      = 'https://github.com/RajsingMore-0151/FoodHub-Restaurant-Website.git'
         GIT_BRANCH    = 'main'
 
         TARGET_USER   = 'ubuntu'
         TARGET_IP     = '172.31.20.47'
 
-        TARGET_PATH   = '/var/www/html/'
+        TARGET_PATH   = '/var/www/html/food'
 
         SSH_CRED_ID   = 'linux'
     }
@@ -24,12 +24,16 @@ pipeline {
             }
         }
 
-        stage('Copy Files to Target Server') {
+        stage('Deploy Website Files') {
             steps {
                 sshagent(credentials: ["${SSH_CRED_ID}"]) {
                     sh """
-                        scp -o StrictHostKeyChecking=no \
-                        -r * \
+                        # Create target directory if it does not exist
+                        ssh -o StrictHostKeyChecking=no ${TARGET_USER}@${TARGET_IP} \
+                        'sudo mkdir -p ${TARGET_PATH}'
+
+                        # Copy website files
+                        scp -o StrictHostKeyChecking=no -r * \
                         ${TARGET_USER}@${TARGET_IP}:${TARGET_PATH}/
                     """
                 }
